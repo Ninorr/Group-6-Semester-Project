7247d724c8452df7bd1e8a646afc697e3f02043a
Neha Koduru <nkoduru@unomaha.edu>
2023-04-22 15:54:02 -0500

    SQKY CLN || Add Home page for user || Neha Koduru

--- assets/catalog/urls.py
+++ assets/catalog/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('aboutus/contactus',views.contactus, name='contactus'),
     path('aboutus/whoweare',views.whoweare, name='whoweare'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path("accounts/signup/", views.signup, name="signup"),
+    path('accounts/signup/', views.signup, name="signup"),
+    path('home', views.home, name="home"),
 ]
